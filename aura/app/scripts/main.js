c407f59d6a061b80e5177d2071ec7ba8da4300d2
Pavel Yashchenko <yashchenkops@gmail.com>
2022-05-18 14:28:47 +0300

    responsive header and footer

--- aura/app/scripts/main.js
+++ aura/app/scripts/main.js
@@ -7,6 +7,13 @@
     }
   });
 
+  (function mobileMenu() {
+    $(".header__burger-button").on("click", function() {
+      $(".header__mobile").toggleClass("is-active");
+      $("body").toggleClass("scroll-block");
+    });
+  })();
+
 	new WOW().init();
 })();
 //\\//\\//\\//\\ End jQuery //\\//\\//\\//\\
