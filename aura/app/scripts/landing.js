014078fc3989269f3a7f8f0e7f21ad0a2e7bd358
Pavel Yashchenko <yashchenkops@gmail.com>
2022-05-19 01:33:16 +0300

    add responsive blocks

--- aura/app/scripts/landing.js
+++ aura/app/scripts/landing.js
@@ -85,6 +85,27 @@ const Accordion = () => {
   }
 }
 
+var swiper = new Swiper(".carousel-price-blocks", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    425: {
+      slidesPerView: 2
+    },
+    640: {
+      slidesPerView: 3
+    },
+    768: {
+      slidesPerView: 4
+    }
+  }
+});
+
+
 (function() {
   Accordion();
 })();
