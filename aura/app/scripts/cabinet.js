d62137e451c3d44e5cd138b21af3201f1e6fc7b6
Pavel Yashchenko <yashchenkops@gmail.com>
2022-06-12 19:48:05 +0300

    [cabinet] added table

--- aura/app/scripts/cabinet.js
+++ aura/app/scripts/cabinet.js
@@ -1,3 +1,12 @@
+// Show more table
+const showMoreTable = () => {
+	$('.table-show-more').on('click', function(){
+    $('.table .tbody__row:nth-child(n+11)').css('display', 'table-row');
+    $('.table').addClass('is-active');
+    $(this).remove();
+  });
+};
+
 (function() {
   // SLiders
   var swiper = new Swiper(".carousel-cabinet-blocks", {
@@ -14,4 +23,6 @@
       draggable: true
     }
   });
+
+  showMoreTable();
 })();
