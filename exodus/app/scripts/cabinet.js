674f5c1e583c74feae9c41003618fc128c650b8a
Pavel Yashchenko <yashchenkops@gmail.com>
2022-06-28 19:21:25 +0300

    added cabinet

--- exodus/app/scripts/cabinet.js
+++ exodus/app/scripts/cabinet.js
@@ -8,21 +8,5 @@ const showMoreTable = () => {
 };
 
 (function() {
-  // SLiders
-  var swiper = new Swiper(".carousel-cabinet-blocks", {
-    direction: "horizontal",
-    slidesPerView: "auto",
-    spaceBetween: 15,
-    grabCursor: true,
-    freeMode: true,
-    mousewheel: false,
-    watchSlidesProgress: true,
-    scrollbar: {
-      el: ".swiper-scrollbar",
-      hide: false,
-      draggable: true
-    }
-  });
-
   showMoreTable();
 })();
